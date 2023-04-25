82da06f3da088d386031ea4c88841758d0a6eb2e
SamiAProjektyWSB <sami.projektyezn@gmail.com>
2023-04-25 21:49:58 +0200

    frontend do strony logowania

--- SoOrganizedApp/account/models.py
+++ SoOrganizedApp/account/models.py
@@ -2,6 +2,24 @@ from django.db import models
 from django.contrib.auth.base_user import BaseUserManager, AbstractBaseUser
 from SoOrganizedApp import settings
 
+
+    
+
+class AccountsManager(BaseUserManager):
+    def create_user(self, mail, nick, password):
+        user = self.model(mail=mail, nick=nick, password=password)
+        user.set_password(password)
+        user.save(using=self.db)
+        return user
+    
+    def create_superuser(self, mail, nick, password):
+        user = self.create_user(mail=mail, nick=nick, password=password)
+        user.is_admin = True
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self.db)
+        return user
+
 class Account(AbstractBaseUser):
     mail = models.EmailField(verbose_name="e-mail", max_length=50, unique=True)
     nick = models.CharField(verbose_name="nick", max_length=30)
@@ -24,20 +42,3 @@ class Account(AbstractBaseUser):
 
     def has_perm(self, perm, obj=None):
         return self.is_admin
-    
-
-class AccountsManager(BaseUserManager):
-    def create_user(self, mail, nick, password):
-        user = self.model(mail=mail, nick=nick, password=password)
-        user.set_password(password)
-        user.save(using=self.db)
-        return user
-    
-    def create_superuser(self, mail, nick, password):
-        user = self.create_user(mail=mail, nick=nick, password=password)
-        user.is_admin = True
-        user.is_staff = True
-        user.is_superuser = True
-        user.save(using=self.db)
-        return user
-
